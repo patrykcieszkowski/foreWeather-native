33134f434205c0e146e7764dea26976d4581b0b4
Patryk Cieszkowski <patryk.cieszkowski@gmail.com>
2017-02-24 16:24:12 +0000

    • removed unnecessary axios library • added GEO API requests actions • added forecast API requests actions

--- app/actions/actionCreators.js
+++ app/actions/actionCreators.js
@@ -1,6 +1,3 @@
-// import axios from 'axios'
-
-
 export function toggleFooterView()
 {
   return { type: 'TOGGLE_FOOTER' }
@@ -36,4 +33,41 @@ export function removeLocation(id)
 export function addLocation(id)
 {
   return { type: 'ADD_LOCATION', payload: { id } }
+}
+
+export function searchLocation(location)
+{
+  let _url = `http://maps.googleapis.com/maps/api/geocode/json?address=${location}&sensor=true`
+
+  return (dispatch) =>
+  {
+    searchLocation_pending()
+    fetch(_url)
+      .then((res) => dispatch(searchLocation_fulfilled(res)) )
+      .catch((e) => dispatch(searchLocation_error(e)) )
+  }
+}
+
+export function searchLocation_pending(res)
+{
+  return { type: 'SEARCH_LOCATION_PENDING' }
+}
+
+export function searchLocation_fulfilled(res)
+{
+  return { type: 'SEARCH_LOCATION_FULFILLED', payload: res.json() }
+}
+
+export function searchLocation_error(error)
+{
+  return { type: 'SEARCH_LOCATION_ERROR', payload: { error } }
+}
+
+export function getForecast(location)
+{
+  let _url = `http://foreweather.heroku.com?location=${location}`
+  return {
+    type: 'FORECAST',
+    payload: fetch(_url).then(res => res.json())
+  }
 }
