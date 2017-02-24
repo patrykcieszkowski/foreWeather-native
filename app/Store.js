849ed19945f2daffeda44f4a7c76293d325855bc
Patryk Cieszkowski <patryk.cieszkowski@gmail.com>
2017-02-24 16:22:42 +0000

    Implemented redux promise middleware

--- app/Store.js
+++ app/Store.js
@@ -1,10 +1,11 @@
 import { createStore, compose, applyMiddleware } from 'redux'
 import ReduxThunk from 'redux-thunk'
+import promise from 'redux-promise-middleware'
 
 import rootReducer from './reducers'
 import defaultState from './state'
 
-const middleware = applyMiddleware(ReduxThunk)
+const middleware = applyMiddleware(promise(), ReduxThunk)
 const store = createStore(rootReducer, defaultState, middleware)
 
 export default store
