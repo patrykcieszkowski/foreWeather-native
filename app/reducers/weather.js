809fa2609741d4a7ddc8ed5192c349bba27b0d66
Patryk Cieszkowski <patryk.cieszkowski@gmail.com>
2017-03-01 03:43:51 +0000

    added 'GET_FORECAST's logic

--- app/reducers/weather.js
+++ app/reducers/weather.js
@@ -4,6 +4,30 @@ export default function(state=weather, action)
 {
   switch (action.type)
   {
+    case 'GET_FORECAST_FULFILLED':
+    {
+      let { json } = action.payload
+
+      let foundIndex = state.findIndex((_weather) => _weather.city.id === json.city.id)
+      let parsedWeather = {
+        city: json.city,
+        list: json.list
+      }
+
+      if (foundIndex > -1)
+      {
+        return [
+          ...state.slice(0, foundIndex),
+          ...state.slice(foundIndex+1),
+          parsedWeather
+        ]
+      }
+
+      return [
+        ...state,
+        parsedWeather
+      ]
+    }
     default:
       return state
   }
